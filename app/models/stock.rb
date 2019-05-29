26b57d3cb967860070e48435105e0f493b42c269
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-05-29 11:47:28 +0200

    adding stocks by user

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,6 @@
 class Stock < ApplicationRecord
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
 
     def self.new_from_lookup(ticker_symbol)
         begin
@@ -7,6 +9,10 @@ class Stock < ApplicationRecord
         rescue Exception => e
             return nil
         end
+    end
+
+    def self.find_by_ticker(ticker_symbol)
+        where(ticker: ticker_symbol).first
     end
     
 end
